b6f81f9caa385bd782adbfa40737fe402798aa27
MayconMaiaDev <gomesmaycon67@gmail.com>
2023-02-22 00:33:41 -0300

    tipos int, float e a funcao type no python

---
+++ logica_python/aula3.py
@@ -0,0 +1,15 @@
+# Tipos int e float 
+# int -. Numero inteiro
+# O tipo int representa qualquer numero
+# positivo ou negativo. Int sem sinal é considerado positivo
+# float - > Número com ponto flutuante
+# O tipo float representa qualquer número positivo ou negativo com ponto flutuante
+# float sem sinal é considerado positivo
+#  a função type mostra o tipo que o python inferiu ao valor
+
+print(11)
+print(-11)
+print(0)
+print(type('Otavio'))
+print(type(0))
+print(type(1,1), type(-1.1), type(0.0))
