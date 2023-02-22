2cec07d0175053a35bbdc6bcf63f2184437b2989
MayconMaiaDev <gomesmaycon67@gmail.com>
2023-02-21 22:17:08 -0300

    identacao do codigo aula2

--- logica_python/aula2.py
+++ logica_python/aula2.py
@@ -6,8 +6,11 @@ str > string -> texto
 Strings são textos que estão dentro de aspas
 """
 print(1234)
-print("Luiz \"Otavio\"")
+
 
 # Apas simples
+print('Luiz')
 # Apas duplas
+print("Luis Otavio")
 # Apas Escape
+print("Luiz \"Otavio\"")
