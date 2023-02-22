838c657df8e31fb6f312b998108827df6fae7456
MayconMaiaDev <gomesmaycon67@gmail.com>
2023-02-21 22:01:08 -0300

    Aula sobre print e comentarios

---
+++ logica_python/aula1.py
@@ -0,0 +1,11 @@
+#escrevendo um comentario
+print('SALVE O CORINTHIANS')
+#escrevendo outro comentario
+print(123)
+print(12, 34, sep='|')
+print(56, 76)
+
+
+
+
+""""""
---
+++ logica_python/aula2.py
@@ -0,0 +1,13 @@
+"""
+Python = linguagem de tipagem dinamica / forte
+
+str > string -> texto
+
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+print("Luiz \"Otavio\"")
+
+# Apas simples
+# Apas duplas
+# Apas Escape
