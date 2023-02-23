4118b5aedd557b0430f8ad1b7e20271d2834aa74
MayconMaiaDev <gomesmaycon67@gmail.com>
2023-02-23 00:06:29 -0300

    Operadores aritimeticos

---
+++ logica_python/aula8.py
@@ -0,0 +1,24 @@
+# Operadores aritiméticos (matematica)
+
+# Desenvolvido por: MayconMaiaDev
+
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 -5
+print('Subtração', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2.2 # sempre o retorno é float
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 2.2 # vai retornar um inteiro
+print('Divisão inteira',divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 5 # Resto da divisão
+print('Modulo', modulo)
