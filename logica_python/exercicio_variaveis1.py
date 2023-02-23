133b16ec2120c2a34b81ac96a7b5dd61c8125333
MayconMaiaDev <gomesmaycon67@gmail.com>
2023-02-22 21:36:31 -0300

    Introducao a variaveis e exercicio de variaveis

---
+++ logica_python/exercicio_variaveis1.py
@@ -0,0 +1,12 @@
+nome = 'Maycon'
+sobrenome = 'Gomes'
+idade = 21
+ano_nascimento = 2001
+maior_idade = idade >=18
+altura_metros = 1.65 
+print('Nome', nome)
+print('Sobrenome:', sobrenome)
+print('Idade', idade)
+print('Ano de nascimento', ano_nascimento)
+print('É maior de idade?', maior_idade)
+print('Altura em metros', altura_metros)
