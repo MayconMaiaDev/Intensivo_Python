cef64ad0004f55cc0304ee1f7a52c1c4439e9d62
MayconMaiaDev <gomesmaycon67@gmail.com>
2023-02-24 23:49:48 -0300

    Aula sobre concatenacao

---
+++ logica_python/aula9.py
@@ -0,0 +1,10 @@
+# Desenvolvido por: MayconMaiaDev
+# Aula sobre concatenação
+
+concatenacao = 'Maycon' + ' ' + 'Gomes'
+print(concatenacao)
+
+b_dez_vezes = 'B' * 10
+tres_vezes_maycon = 3 * 'Maycon'
+print(b_dez_vezes)
+print(tres_vezes_maycon)
