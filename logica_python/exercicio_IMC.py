dcf42cecf414a83e2e4e4063a8b222b36e528da9
MayconMaiaDev <gomesmaycon67@gmail.com>
2023-02-25 00:03:40 -0300

    exercicio de calculo de IMC

---
+++ logica_python/exercicio_IMC.py
@@ -0,0 +1,11 @@
+# Desenvolvido por: MayconMaiaDev
+# Desafio do calculo de IMC
+
+nome = 'Maycon'
+altura = 1.65
+peso = 76
+imc = peso / altura ** 2
+
+print(nome, 'tem isso', altura, 'de altura,',)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
