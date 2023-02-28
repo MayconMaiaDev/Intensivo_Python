5afc7fc727eea4b4436e1ae8c7b19e274c236f00
MayconMaiaDev <gomesmaycon67@gmail.com>
2023-02-28 16:44:20 -0300

    Elif if else

---
+++ logica_python/aula15.py
@@ -0,0 +1,26 @@
+# Desenvolvido por MayconMaiaDev
+
+# if / elif      / else
+# se / se não se / se não
+
+valor1 = True
+valor2 = True
+valor3 = True
+valor4 = True
+
+if valor1:
+    print('Código para condição 1')
+    print('Código para condição 1')
+elif valor2:
+    print('Código para condição 2')
+elif valor3:
+    print('Código para condição 3')
+elif valor4:
+    print('Código para condição 4')
+else:
+    print('Nenhuma condição foi satisfeita.')
+
+if 10 == 10:
+    print('Outro if')
+
+print('Fora do if')
