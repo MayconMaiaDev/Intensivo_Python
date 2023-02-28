8cd6d44ea3a4aa67e37173088b8bc4246ef47fec
MayconMaiaDev <gomesmaycon67@gmail.com>
2023-02-28 18:02:46 -0300

    Operadores de comparacao relacional

---
+++ logica_python/aula16.py
@@ -0,0 +1,18 @@
+"""
+Desenvolvido por: MayconMaiaDev
+Operadores de comparação (relacionais)
+OP      Significado         Exemplo (True)
+>       maior               2 > 1
+>=      maior ou igual      2 >= 2
+<       menor               1 < 2
+<=      menor ou igual      2 <= 2
+==      igual               'a' == 'a'
+!=      diferente           'a' != 'b'
+"""
+maior = 5 > 1
+maior_ou_igual = 4 >= 4
+menor = 2 < 9
+menor_ou_igual = 7 <= 7
+igual = 'm' == 'm'
+diferente = 'm' != 'a'
+print('palmeiras não tem mundial!')
