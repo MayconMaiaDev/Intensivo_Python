8a389975b856cacbf2c81f47d1a54e04b3d94a4f
MayconMaiaDev <gomesmaycon67@gmail.com>
2023-03-01 12:56:48 -0300

    Operador logico and

---
+++ logica_python/aula17.py
@@ -0,0 +1,20 @@
+# Desenvolvido por: MayconMaiaDev
+# Operadores lógicos
+# and (e)
+# and - Todas as condições precisam serverdadeiras.
+# Se qualquer valor for considerado falso,
+# a expressão inteira será avaliada naquele valor
+# São considerados falsy (que vc já viu)
+# 0 0.0 '' False
+# Também existe o tipo None que é
+# usado para representar um não valor
+
+entrada = input('[E]ntrar [S]air: ')
+senha_digitada = input('Senha: ')
+
+senha_permitida = '2023'
+
+if entrada == 'E' and senha_digitada == senha_permitida:
+    print('Entrar')
+else:
+   print('Sair')
